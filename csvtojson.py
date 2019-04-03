b8b924baba6694428fc24b464bac39db07ae59ab
Charlie <charlie@ML-train.local>
2019-04-03 11:06:02 -0400

    Created Json Files Created json files from csv using the csvtojson script i wrote

--- csvtojson.py
+++ csvtojson.py
@@ -3,27 +3,35 @@
 #Author: Charlie Chiccarine
 #Email: src322@drexel.edu
 #
-#Last Edited: 4/2/2019
-# Created the script
+#Last Edited: 4/3/2019
+# Altered to work on directories
 
+import os
 import sys
 
-def createJSONFileName(csvName):
-	temp = csvName
-	temp = temp.split(".")
+def createJSONFileName(csvPath):
+	temp = csvPath
+	temp = temp.split("/")
+	csvName = temp.pop()
+	dirName = ""
+	if len(temp) != 0:
+		for sub in temp:
+			dirName += sub + "/"
+	else:
+		dirName = "./"
+	
+	temp = csvName.split(".")
 	temp.pop()
+	
 	jsonName = ""
 	for sub in temp:
 		jsonName += sub + "."
 	jsonName += "json"
+
+	jsonName = dirName + jsonName
 	return jsonName
 
-if __name__ == "__main__":
-	if len(sys.argv) != 1:
-		csvFileName = sys.argv[1]
-	else:
-		csvFileName = input("CSV File:")
-
+def convertCSV(csvFileName):
 	jsonFileName = createJSONFileName(csvFileName)
 
 	csvFile = open(csvFileName)
@@ -37,7 +45,8 @@ if __name__ == "__main__":
 		csvFile[i] = csvFile[i].split(",")
 	
 	jsonFile.write("[")
-	for i in range(1, len(csvFile)):
+	for i in range(len(csvFile) - 1):
+		i += 1
 		if i != 1:
 			jsonFile.write(",")
 
@@ -45,10 +54,40 @@ if __name__ == "__main__":
 		for j in range(len(csvFile[i])):
 			if j != 0:
 				jsonFile.write(",")
-
 			jsonFile.write("\"" + csvFile[0][j] + "\" : \"" + csvFile[i][j] + "\"")
 		jsonFile.write("}")
 	jsonFile.write("]")
 
-	print("Completed")
 	print("The JSON file for", csvFileName, "is at", jsonFileName)
+
+if __name__ == "__main__":
+	onDir = False
+
+	if len(sys.argv) != 1:		
+		if sys.argv[1] == "-d":
+			if len(sys.argv) < 3:
+				print( "Incorrect Usage" )
+				print( "For individual file:" )
+				print( "  python3 csvtojson.py [fileName.csv]" )
+				print( "For all .csv files in a directory:" )
+				print( "  python3 csvtojson.py -d [directoryName]" )
+				sys.exit()
+			
+			dirName = sys.argv[2]	
+			onDir = True
+		else:	
+			csvFileName = sys.argv[1]
+	else:
+		csvFileName = input("CSV File:")
+
+	if onDir:
+		for csv in os.listdir( dirName ):
+			if csv[-4:] != ".csv":
+				continue
+			pathName = dirName + "/" + csv
+			convertCSV(pathName)
+
+	else:
+		convertCSV(csvFileName)
+
+	print( "Completed" )
